1e9194a0f673c96c734ae6e6087a1e44c90bf8a2
Tebesi <kabuye_frank@yahoo.com>
2019-07-03 17:18:45 +0100

    age is showing number

--- src/ControlledForm.js
+++ src/ControlledForm.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
-import FormRender from './FormRender';
+import React, { Component } from "react";
+import FormRender from "./FormRender";
 
 export class ControlledForm extends Component {
-    state = {
-        firstName: "",
-        lastName: "",
-        age: null,
-        Student: false,
-        gender:"",
-        email: "",
-        school:""
+  state = {
+    firstName: "",
+    lastName: "",
+    age: null,
+    Student: false,
+    gender: "",
+    email: "",
+    school: ""
+  };
+  handleChange = e => {
+    const { name, value, type, checked } = e.target;
+    if (name === "age") {
+      if (value !== Number) {
+        alert("Age MUST be a number!!");
+      }
     }
-    handleChange = (e) => {
-        const { name, value, type, checked } = e.target
-        if (name === "age") {
-            if (!Number(value)) {
-                alert("Your age must be a number")
-            }
-        }
-        type==="checkbox"?this.setState({name:checked}):this.setState({
-            [name]:value
-        })
-    }
+    type === "checkbox"
+      ? this.setState({ name: checked })
+      : this.setState({
+          [name]: value
+        });
+  };
   render() {
-    return (
-        <FormRender handleChange={this.handleChange} {...this.state}/>
-    )
+    return <FormRender handleChange={this.handleChange} {...this.state} />;
   }
 }
 
-export default ControlledForm
+export default ControlledForm;
--- src/FormRender.js
+++ src/FormRender.js
@@ -1,43 +1,93 @@
-import React from 'react'
+import React from "react";
 
 function FormRender(props) {
   return (
     <main>
-    <form>
-        <input type="text"name="firstName"placeholder="First Name" value={props.firstName}
-            onChange={props.handleChange} />
-              <br />
-              <br/>
-        <input type="text" name="lastName"placeholder="Last Name" value={props.lastName}
-            onChange={props.handleChange} />
-              <br />
-              <br/>
-        <input type="text" name="age"placeholder="Age" value={props.age}
-            onChange={props.handleChange} />
-        <br/>
-       <label><input type="checkbox" name="student" checked={props.student}
-            onChange={props.handleChange} />Student</label>
-        <br/>
-        <label><input type="radio" name="gender" value="male"checked={props.gender === "male"}
-            onChange={props.handleChange} />Male</label>
-        <br/>
-         <label><input type="radio" value="female"name="gender" checked={props.gender === "female"}
-            onChange={props.handleChange} />Female</label>
+      <form>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          value={props.firstName}
+          onChange={props.handleChange}
+        />
         <br />
-        <input type="text" name="email"placeholder="Email" value={props.email}
-            onChange={props.handleChange} />
         <br />
-        <input type="text" name="school"placeholder="School" value={props.school}
-            onChange={props.handleChange} />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          value={props.lastName}
+          onChange={props.handleChange}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          name="age"
+          placeholder="Age"
+          value={props.age}
+          onChange={props.handleChange}
+        />
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="student"
+            checked={props.student}
+            onChange={props.handleChange}
+          />
+          Student
+        </label>
+        <br />
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={props.gender === "male"}
+            onChange={props.handleChange}
+          />
+          Male
+        </label>
+        <br />
+        <label>
+          <input
+            type="radio"
+            value="female"
+            name="gender"
+            checked={props.gender === "female"}
+            onChange={props.handleChange}
+          />
+          Female
+        </label>
+        <br />
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={props.email}
+          onChange={props.handleChange}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          name="school"
+          placeholder="School"
+          value={props.school}
+          onChange={props.handleChange}
+        />
         <hr />
         <h1>Entered Information:</h1>
-        <h3>NAME:{props.firstName} {props.lastName}</h3>
+        <h3>
+          NAME:{props.firstName} {props.lastName}
+        </h3>
         <p>I am {props.age} years old</p>
         <p>My school is {props.school}</p>
-        
-    </form>
-</main>
-  )
+      </form>
+    </main>
+  );
 }
 
-export default FormRender
+export default FormRender;
